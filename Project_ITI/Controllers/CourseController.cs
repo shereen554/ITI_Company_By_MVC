8ee8f6321402922074b63e4db4d3ee777e78f621
Shereen Ahmed <Shereenahmed387@gmail.com>
2025-02-05 19:05:28 +0200

    Add New Course

---
+++ Project_ITI/Controllers/CourseController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_ITI.Models;
+using Project_ITI.ViewModel;
+
+namespace Project_ITI.Controllers
+{
+    public class CourseController : Controller
+    {
+        ITIContext db = new ITIContext();
+
+        public List<CourseWithDepartmentViewModel> AllCourse()
+        {
+            List<CourseWithDepartmentViewModel> viewModels = new List<CourseWithDepartmentViewModel>();
+            var course = db.Courses.ToList();
+            foreach (var courseViewModel in course)
+            {
+                CourseWithDepartmentViewModel viewModel = new CourseWithDepartmentViewModel()
+                {
+                    Name = courseViewModel.Name,
+                    Degree = courseViewModel.Degree,
+                    MinDegree = courseViewModel.MinDegree,
+                    Hours = courseViewModel.Hours,
+                    DepartmentId = courseViewModel.DepartmentId,
+                };
+                viewModel.DepartmentName = db.Departments.SingleOrDefault(n => n.Id == courseViewModel.DepartmentId).Name;
+                viewModel.departments = db.Departments.ToList();
+                viewModels.Add(viewModel);
+            }
+            return viewModels;
+        }
+        public IActionResult Index()
+        {
+            return View("Index",AllCourse());
+            
+        }
+        [HttpGet]
+        public IActionResult New()
+        {
+            CourseWithDepartmentViewModel viewModel = new CourseWithDepartmentViewModel();
+            viewModel.departments=db.Departments.ToList();
+
+            return View("New", viewModel);
+        }
+        [HttpPost]
+        public IActionResult SaveNew(CourseWithDepartmentViewModel viewModel) 
+        {
+            
+            if(viewModel.Degree !=null && viewModel.MinDegree !=null &&  viewModel.Hours !=null && viewModel.Name !=null)
+            {
+                Course course = new Course()
+                {
+                    Name=viewModel.Name,
+                    Degree=viewModel.Degree,
+                    Hours=viewModel.Hours,
+                    MinDegree=viewModel.MinDegree,
+                    DepartmentId=viewModel.DepartmentId,
+                };
+                db.Courses.Add(course);
+                db.SaveChanges();
+                return RedirectToAction("Index",AllCourse());
+            }
+            viewModel.departments=db.Departments.ToList();
+            return View("New",viewModel);
+        }
+    }
+}
---
+++ Project_ITI/ViewModel/CourseWithDepartmentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Project_ITI.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_ITI.ViewModel
+{
+    public class CourseWithDepartmentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Degree { get; set; }
+        public decimal MinDegree { get; set; }
+        public int Hours { get; set; }
+        public int? DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public List<Department> departments { get; set; }
+
+    }
+}
